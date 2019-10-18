5a2044f80432a4546145dac43c00c89fb1d98ace
Alden Goldstein <alden.goldstein@Aldens-MBP.home>
2019-10-17 22:54:56 -0400

    WIP push logic and node allocation almost done

---
+++ algo_notes.md
@@ -0,0 +1,19 @@
+## Reference Counting
+- P Norm, C Norm = V --> P XCHG t+2 -> t, C READS t+2, C WRITES ref+1
+- C Norm, P Norm = V --> C READS t, C WAITS, P XCHG t+2 -> t, C READS t+2, C WRITES ref+1
+- P ABAd, C Norm = I --> P XCHG t+1 -> t, C READS t+1, C WRITES t+2, C WRITES ref+1
+- C Norm, P ABAd = I --> C READS t, C WAITS, P XCHG t+1 -> t, C READS t+1, C WRITES t+2, C WRITES ref+1
+- P Norm, C ABAd = V --> P XCHG t+2 -> t, C XCHG t+1 -> t+2, C WRITES t+2, C WRITES ref+1
+- C ABAd, P Norm = I --> C XCHG t+1 -> t, P XCHG t+2 -> t+1, P WRITES ref+1
+- P ABAd, C ABAd = I --> P XCHG t+1 -> t, C XCHG t+1 -> t+1, C WRITES t+2, C WRITES ref+1
+- C ABAd, P ABAd = I --> C XCHG t+1 -> t, P XCHG t+1 -> t+1, P WRITES t+2, P WRITES ref+1
+
+## Designated Releaser 
+- P Norm, C Norm = V --> P XCHG t+1 -> t, C READS t+1, C WRITES t+2
+- C Norm, P Norm = V --> C READS t, C WAITS, P XCHG t+1 -> t, C READS t+1, C WRITES t+2
+- P ABAd, C Norm = I --> P XCHG t+2 -> t, C READS ANY
+- C Norm, P ABAd = I --> C READS t, C WAITS, P XCHG t+2 -> t, C READS ANY
+- P Norm, C ABAd = V --> P XCHG t+1 -> t, C CMPXCHG t,t+1 -> t+1, C WRITES t+2
+- C ABAd, P Norm = I --> C CMPXCHG t,t+1 -> t, P XCHG t+1 -> t+1, P WRITES t+2
+- P ABAd, C ABAd = I --> P XCHG t+2 -> t, C CMPXCHG t,t+1 -> ANY
+- C ABAd, P ABAd = I --> C CMPXCHG t,t+1 -> t, P XCHG t+2 -> t+1
--- mpmc.h
+++ mpmc.h
@@ -20,32 +20,31 @@ public:
     MPMCQueue(const MPMCQueue &) = delete;
     MPMCQueue &operator=(const MPMCQueue &) = delete;
 
-    template <typename... Args> void emplace(Args &&... args) noexcept {
+    void push(T &&v) noexcept {
     }
 
-    template <typename... Args> bool try_emplace(Args &&... args) noexcept {
-    }
+    bool try_push(T &&v) noexcept {
+        auto success = false; 
+        auto ptr = tail_.load(A) 
 
-    void push(const T &v) noexcept {
-        emplace(v);
-    }
-
-    template <typename P,
-            typename = typename std::enable_if<
-                std::is_nothrow_constructible<T, P &&>::value>::type>
-    void push(P &&v) noexcept {
-        emplace(std::forward<P>(v));
-    }
-
-    bool try_push(const T &v) noexcept {
-        return try_emplace(v);
-    }
-
-    template <typename P,
-            typename = typename std::enable_if<
-                std::is_nothrow_constructible<T, P &&>::value>::type>
-    bool try_push(P &&v) noexcept {
-        return try_emplace(std::forward<P>(v));
+        while (!success) {
+            auto &node = nodes[ptr.idx]
+            auto const enq_idx = node.enq_idx.fetch_add(1, A)
+            if (enq_idx < blocksize_) {
+                // there is room for us, try pushing to slot
+                success = push_to_slot(
+                        std::forward<T>(v),
+                        node.slots[enq_idx],
+                        node,
+                        ptr.tag);
+            } else {
+                auto const curr_next = node.next.load(X)
+                if (curr_next.idx != kNull)
+                    divider_.compare_exchange_strong(ptr, curr_next, R, A);
+                else if (!get_next_tail(ptr))
+                    return false
+            }
+        }
     }
 
     void pop(T &v) noexcept {
@@ -54,29 +53,59 @@ public:
     bool try_pop(T &v) noexcept {
     }
 
+private
+    template <typename U>
+    bool push_to_slot(
+            U &&v,
+            Slot& slot,
+            Node& node,
+            uint32_t ptr_state) noexcept {
+
+        if (node.state == ptr_state) {
+            // everything looks good, attempt to write
+            slot.item = std::forward<U>(v);
+            auto const prev_state = slot.state.exchange(node.state + 1, R);
+            if (prev_state == node.state) 
+                return true // success!
+
+            // consumer ABA occurred, recover item and close state 
+            v = std::move(slot.item);
+            slot.state.store(node.state + 2, R);
+        } else {
+            // producer ABA occurred, invalidate slot
+            slot.state.exchange(node.state + 2, R);
+        }
+
+        return false
+    }
+
+    bool get_next_tail(TaggedPtr &curr_tail) noexcept {
+        // first try to advance tail
+        auto const new_tail = nodes[curr_tail.idx].next.load(A) 
+        TaggedPtr null_ptr = {kNull, curr_tail.tag}
+        if (new_tail != null_ptr) {
+            tail_.compare_exchange_strong(curr_tail, new_tail, R, A)
+            return true
+        }
+ 
+        auto const alloc_idx = freelist_.try_pop(curr_tail);
+        if (curr_tail.idx == kNull) {
+            curr_tail = tail_.load(A)
+            return true
+        }
+        else if (alloc_idx == kNull)
+            return false // freelist is empty
+
+
+    }
+
 private:
     static constexpr size_t kCacheLineSize = 128;
-    static constexpr size_t kNull = 0xFFFFFFFF;
+    static constexpr uint32_t kNull = 0xFFFFFFFF;
 
-    struct Slot {
-        ~Slot() noexcept {
-            if (turn & 1) {
-                destroy();
-            }
-        } 
-
-        template <typename... Args> void construct(Args &&... args) noexcept {
-            new (&storage) T(std::forward<Args>(args)...);
-        }
-
-        void destroy() noexcept {
-            reinterpret_cast<T *>(&storage)->~T();
-        }
-
-        T &&move() noexcept { return reinterpret_cast<T &&>(storage); }
-
-        alignas(kCacheLineSize) std::atomic<size_t> turn = {0};
-        typename std::aligned_storage<sizeof(T), alignof(T)>::type storage;
+    struct alignas(kCacheLineSize) Slot {
+        std::atomic<uint32_t> state = 0;
+        T item;
     };
 
     struct TaggedPtr {
@@ -85,87 +114,56 @@ private:
     };
 
     struct Node {
-        explicit Node() {
-            size_t space = blocksize_ * sizeof(Slot) + kCacheLineSize - 1;
-            buffer = malloc(space);
-            if (buffer == kNull) {
-                throw std::bad_alloc();
-            }
-
-            slots = reinterpret_cast<Slot *>(
-                    std::align(
-                        kCacheLineSize,
-                        blocksize_ * sizeof(Slot),
-                        buffer,
-                        space));
-            if (slots == kNull) {
-                free(buffer);
-                throw std::bad_alloc();
-            }
-
-            for (size_t i = 0; i < blocksize_; ++i) {
-                new (&slots[i]) Slot();
-            } 
-        }
-
-        ~Node() {
-            for (size_t i = 0; i < blocksize_; ++i) {
-                reinterpret_cast<Slot *>(&slots[i])->~Slot();
-            } 
- 
-            free(buffer)
-        }
+        Node() : slots(new Slots[blocksize_]) {}
+        ~Node() { delete[] slots }
         
-        void *buffer;
         Slot *slots;
-        alignas(kCacheLineSize) std::atomic<TaggedPtr> next;
-        alignas(kCacheLineSize) std::atomic<uint32_t> free_next;
-        alignas(kCacheLineSize) std::atomic<uint64_t> enq_idx;
-        alignas(kCacheLineSize) std::atomic<uint64_t> deq_idx;
-        alignas(kCacheLineSize) std::atomic<uint64_t> ref_cnt;
+        alignas(kCacheLineSize) state = 0;
+        
+        // atomics
+        alignas(kCacheLineSize) std::atomic<TaggedPtr> next = {0, 0};
+        alignas(kCacheLineSize) std::atomic<uint32_t> free_next = 0;
+        std::atomic<size_t> ref_cnt = 0; // same cacheline as free next
+        alignas(kCacheLineSize) std::atomic<size_t> enq_idx = 0;
+        alignas(kCacheLineSize) std::atomic<size_t> deq_idx = 0;
     };
 
-	struct FreeList {
-		void add(uint32_t new_idx) {
-			TaggedPtr curr_head = head.load(std::memory_order_relaxed);
-			TaggedPtr new_head = { new_idx };
-			do {
-				new_head.tag.store(curr_head.tag, std::memory_order_relaxed)
-                nodes[new_idx].free_next.store(curr_head.idx, std::memory_order_relaxed);
-			} while (!head.compare_exchange_weak(
-                        curr_head,
-                        new_head,
-                        std::memory_order_release,
-                        std::memory_order_relaxed));
-		}
+    struct FreeList {
+        void push(uint32_t new_idx) {
+            auto const curr_head = head.load(X);
+            TaggedPtr new_head = {new_idx};
+            do {
+                new_head.tag.store(curr_head.tag, X)
+                nodes[new_idx].free_next.store(curr_head.idx, X);
+            } while (!head.compare_exchange_weak(curr_head, new_head, R, X));
+        }
 
-		Node * try_get(Node &q_tail) {
-			TaggedPtr curr_head = head.load(std::memory_order_acquire);
-			TaggedPtr new_head;
-			
-            // check for both an empty free list AND whether we need to still allocate
-            while (
-                    curr_head.idx != kNull && 
-                    q_tail.next.load(std::memory_order_relaxed).idx == kNull) {
-                
-                new_head.idx = nodes[curr_head.idx].free_next.load(std::memory_order_relaxed);
-				new_head.tag = curr_head.tag + 1;
-				if (head.compare_exchange_weak(
-                            curr_head,
-                            new_head,
-                            std::memory_order_release,
-                            std::memory_order_acquire)) {
-					break;
-				}
-			}
-			return curr_head.idx;
-		}
+        uint32_t try_pop(TaggedPtr &q_tail) {
+            auto const curr_head = head.load(A);
+            auto const &q_tail_node = nodes[q_tail.idx] 
+            TaggedPtr null_ptr = {kNull, q_tail.tag};
+            TaggedPtr new_head;
+           
+            // check for empty list on each loop iteration
+            while (curr_head.idx != kNull) {
+                new_head.idx = nodes[curr_head.idx].free_next.load(X);
+                new_head.tag = curr_head.tag + 1;
+                if (head.compare_exchange_weak(curr_head, new_head, R, A))
+                    break; // success!
+                else if (q_tail_node.next.load(X) != null_ptr) { 
+                    q_tail.idx = kNull // indicate allocation is no longer needed
+                    break;
+                }
+            }
+            return curr_head.idx;
+        }
 
-		std::atomic<TaggedPtr> head;
-	};
+        std::atomic<TaggedPtr> head;
+    };
 
 private:
     size_t blocksize_; 
-    alignas(kCacheLineSize) std::atomic<uint32_t> head_;
-    alignas(kCacheLineSize) std::atomic<uint32_t> tail_;
+    alignas(kCacheLineSize) std::atomic<TaggedPtr> head_;
+    alignas(kCacheLineSize) std::atomic<TaggedPtr> tail_;
+    alignas(kCacheLineSize) std::atomic<TaggedPtr> lead_;
 };
